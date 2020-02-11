b00b3c49b53fe496fb6b03aadc56ad98b2df2270
Tobias Midskard <tobias@wecode.dk>
2020-02-11 20:59:21 +0100

    Added three-page application

---
+++ week6/Company Business-card/Company Business-card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Company Business-card
+//
+//  Created by tobiasmidskard on 11/02/2020.
+//  Copyright © 2020 tobiasmidskard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ week6/Company Business-card/Company Business-card/WebViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WebViewController.swift
+//  Company Business-card
+//
+//  Created by tobiasmidskard on 11/02/2020.
+//  Copyright © 2020 tobiasmidskard. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://orsted.dk/")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
