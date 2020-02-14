7bf3870b6304b6d53ac6c251e3a7f45dc9049b6a
Tobias Midskard <tobias@wecode.dk>
2020-02-14 13:31:16 +0100

    .

--- week7/MyNotebook/MyNotebook/ViewController.swift
+++ week7/MyNotebook/MyNotebook/ViewController.swift
@@ -19,18 +19,28 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     var textArray = [String]()
     
+    let fileName = "theString.txt"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        textArray.append("Hello")
-        textArray.append("How are you")
+        textArray.append("Hellooooo")
         
         myTextView.text = savedText
         
         tableView.dataSource = self
         tableView.delegate = self
+ 
     }
 
+    @IBAction func deleteButtonPressed(_ sender: Any) {
+        if (currentIndex != -1) {
+            textArray.remove(at: currentIndex)
+            tableView.reloadData()
+            currentIndex = -1
+        }
+    }
+    
     @IBAction func buttonPressed(_ sender: Any) {
         savedText = myTextView.text!
         if (savedText != "") {
@@ -42,7 +52,37 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             }
             tableView.reloadData()
             myTextView.text = ""
+            saveStringToFile(str: savedText, fileName: fileName)
+            print(readStringFromFile(fileName: fileName))
         }
+    }
+    
+    func saveStringToFile(str:String, fileName:String) {
+        let filePath = getDocumentDir().appendingPathComponent(fileName)
+        
+        do {
+            try str.write(to: filePath, atomically: true, encoding: .utf8)
+            print("OK writing string ' \(str) ' to file")
+        } catch {
+            print("ERROR writing string ' \(str) ' to file")
+        }
+    }
+    
+    func readStringFromFile(fileName:String) -> String {
+        let filePath = getDocumentDir().appendingPathComponent(fileName)
+        
+        do {
+            let string = try String(contentsOf: filePath, encoding: .utf8)
+            return string
+        } catch {
+            print("ERROR while reading \(fileName)")
+        }
+        return "empty"
+    }
+    
+    func getDocumentDir() -> URL {
+        let documentDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return documentDir[0]
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -61,6 +101,5 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         myTextView.text = textArray[indexPath.row]
         currentIndex = indexPath.row
     }
-    
 }
 
