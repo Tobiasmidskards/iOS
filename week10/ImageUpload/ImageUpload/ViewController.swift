df81aaeb178a91545feea1a24e2072ffeccef5ca
Tobias Midskard <tobias@wecode.dk>
2020-03-18 23:25:54 +0100

    imagepicker and download

--- week10/ImageUpload/ImageUpload/ViewController.swift
+++ week10/ImageUpload/ImageUpload/ViewController.swift
@@ -9,21 +9,55 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var chooseImageButton: UIButton!
+    let imagePicker = UIImagePickerController()
     
     let storage = Storage.storage(url:"gs://my-personal-notes-2a1d8.appspot.com")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getImage()
+    }
+    
+    @IBAction func chooseImageButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            print("Button capture")
+
+            imagePicker.delegate = self
+            imagePicker.sourceType = .savedPhotosAlbum
+            imagePicker.allowsEditing = true
+
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject: AnyObject]!){
+        self.dismiss(animated: true, completion: { () -> Void in
+
+        })
+
+        imageView.image = image
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+
+        guard let image = info[.editedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+
+        imageView.image = image
         uploadImage(image: imageView.image!)
     }
     
     func uploadImage(image: UIImage) {
         let storageRef = storage.reference()
-        let folderRef = storageRef.child("images/apple.png")
+        let folderRef = storageRef.child("images/latest.png")
         
         if let imageData = image.jpegData(compressionQuality: 0.1) {
             
@@ -44,7 +78,20 @@ class ViewController: UIViewController {
             }
         }
     }
-
-
+    
+    func getImage() {
+        let storageRef = storage.reference()
+        let folderRef = storageRef.child("images/latest.png")
+        
+        folderRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+          if let error = error {
+            // Uh-oh, an error occurred!
+          } else {
+            // Data for "images/island.jpg" is returned
+            let image = UIImage(data: data!)
+            self.imageView.image = image
+          }
+        }
+    }
 }
 
